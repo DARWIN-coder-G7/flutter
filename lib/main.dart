1bf2dbd1528cb7a3d7e7f6c04329bb6106c42ba3
Darwin_S <darwin.s@willwaretech.com>
2023-11-16 05:13:22 +0000

    api call alone

--- lib/main.dart
+++ lib/main.dart
@@ -1,172 +1,42 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<void> fetchdata() async {
+  final res = await http.get(Uri.parse("https://reqres.in/api/users?page=2"));
+  print("object");
+  print(res.statusCode);
+  if (res.statusCode == 200) {
+    print(res.body.toString());
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    fetchdata();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: 170,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black54,
-                    offset: Offset(4, 5),
-                    blurRadius: 6,
-                  )
-                ],
-                color: Color(0xffdeb174),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(35),
-                  bottomRight: Radius.circular(35),
-                ),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    height: 100,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black54,
-                          offset: Offset(4, 5),
-                          blurRadius: 6,
-                        )
-                      ],
-                      color: Colors.black,
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://images.unsplash.com/photo-1617083278519-75734faa66b2?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2FyJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        fillColor: Colors.white,
-                        filled: true,
-                        hintText: "Search Crackers ...",
-                        suffixIcon: Icon(Icons.search, size: 30),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              height: 450,
-              color: Colors.white12,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Stylish Cars",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          "View All",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
-                            color: Colors.blue,
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 150,
-                      width: double.infinity,
-                      //color: Colors.red,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            buildContainer_car("Lamborghini \n \$.70,000",
-                                "https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/HuracanModelImage.jpg"),
-                            SizedBox(width: 10),
-                            buildContainer_car("Ferrari \n \$.20,000",
-                                "https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/HuracanModelImage.jpg"),
-                            SizedBox(width: 10),
-                            buildContainer_car("Rolls Royce \n \$.30,000",
-                                "https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/HuracanModelImage.jpg"),
-                            SizedBox(width: 10),
-                            buildContainer_car("BMW \n \$.50,000",
-                                "https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/HuracanModelImage.jpg"),
-                            SizedBox(width: 10),
-                            buildContainer_car("BMW Black \n \$.60,000",
-                                "https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/HuracanModelImage.jpg"),
-                            SizedBox(width: 10),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
+        body: Center(
+          child: Text(
+            "Hello from API",
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-    );
-  }
-
-  Container buildContainer_car(text, img) {
-    return Container(
-      height: 150,
-      width: 110,
-      decoration: BoxDecoration(
-          color: Color(0xff8896a2), borderRadius: BorderRadius.circular(15)),
-      child: Column(
-        children: [
-          Container(
-            height: 80,
-            width: 110,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
-              image:
-                  DecorationImage(image: NetworkImage(img), fit: BoxFit.cover),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            text,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 17,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-          )
-        ],
       ),
     );
   }
